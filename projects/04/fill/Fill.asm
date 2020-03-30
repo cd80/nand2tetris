30cbf92e3bdc49c7b0c55edb25932353f6aa237e
unknown <rkwk0112@gmail.com>
2020-03-30 10:18:05 +0900

    Finish Week 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,117 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+	// initialize variables
+	// @size = 8191(from 0)
+	@8191
+	D=A
+	@size
+	M=D
+	// @idx = 0
+	@idx
+	M=0
+	// @fill = 0(/-1)
+	@fill
+	M=0
+	// @RET used when calling (FILL_SCREEN)
+	@RET
+	M=0
+
+	// initialize program
+	@KEY_NOT_PRESSED
+	0;JMP
+
+(FILL_SCREEN)
+	// set @idx to @size
+	@size
+	D=M
+	@idx
+	M=D
+
+	(LOOP)
+	// pointer = @SCREEN + idx
+	@SCREEN
+	D=A
+	@pointer
+	M=D
+	@idx
+	D=M
+	@pointer
+	M=M+D
+
+	// fill pointer with @fill
+	@fill
+	D=M
+	@pointer
+	A=M
+	M=D
+
+	// decrease idx and goto LOOP
+	// jmp to LOOPEND if idx+1 == 0
+	@idx
+	MD=M-1
+	@LOOPEND
+	D+1;JEQ
+
+	@LOOP
+	0;JMP
+
+	(LOOPEND)
+
+	@RET
+	A=M
+	0;JMP
+
+(KEY_NOT_PRESSED)
+	// clear SCREEN and loop while @KBD != 0
+	
+	// clear SCREEN
+	@fill
+	M=0
+
+	// set RET
+	@KNP_AFTERFILL
+	D=A
+	@RET
+	M=D
+
+	// call FILL_SCREEN(will return to KMP_AFTERFILL)
+	@FILL_SCREEN
+	0;JMP
+
+	(KNP_AFTERFILL)
+	@KBD
+	D=M
+	@KEY_PRESSED
+	D;JNE
+	@KNP_AFTERFILL
+	0;JMP
+	// loop and check @KBD != 0, if KBD != 0, goto KEY_PRESSED
+
+
+(KEY_PRESSED)
+	// fill SCREEN and loop while @KBD == 0
+
+	// fill SCREEN
+	@fill
+	M=-1
+
+	@KP_AFTERFILL
+	D=A
+	@RET
+	M=D
+
+
+	// call FILL_SCREEN(will return to KP_AFTERFILL)
+	@FILL_SCREEN
+	0;JMP
+
+	(KP_AFTERFILL)
+	// loop and check @KBD == 0, if KBD == 0, goto KEY_NOT_PRESSED
+	@KBD
+	D=M
+	@KEY_NOT_PRESSED
+	D;JEQ
+	@KP_AFTERFILL
+	0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,46 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// set R2 = 0
+@0
+D=A
+@R2
+M=D
+
+// store R1 somewhere else(@times)
+@R1
+D=M
+@times
+M=D
+
+// loop while @times == 0, @R2 = @R2 + @R0
+(LOOP)
+	// if @times == 0: goto LOOPEND
+	@times
+	D=M
+	@LOOPEND
+	D;JEQ
+
+	// if @R0 == 0: goto LOOPEND
+	@R0
+	D=M
+	@LOOPEND
+	D;JEQ
+
+	@R0
+	D=M
+	@R2
+	M=M+D
+
+	@LOOPEND
+	D;JEQ
+
+	@times
+	M=M-1
+	@LOOP
+	0;JMP
+
+(LOOPEND)
+	@LOOPEND
+	0;JMP
